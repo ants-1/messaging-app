cbcb2422ad92b6c535281629ed725a253236c505
Anthony <anthonysalisbury98@outlook.com>
2024-07-13 09:58:15 +0100

    Refactor messageController

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,129 +1,139 @@
-import Message from '../models/Message.js';
-import Chat from '../models/Chat.js'
-import asyncHandler from 'express-async-handler';
+import Message from "../models/Message.js";
+import Chat from "../models/Chat.js";
+import asyncHandler from "express-async-handler";
 
 // @desc    get all messages from chat
-// route    GET /chat/:chatId/messages
+// route    GET /chats/:chatId/messages
 const get_all_messages = asyncHandler(async (req, res, next) => {
-    const { chatId } = req.params;
-    const chat = await Chat.findById(chatId).populate('Message').exec();
+  const { chatId } = req.params;
+  const chat = await Chat.findById(chatId).populate('messages').exec();
 
-    const messages = chat.messages;
+  const messages = chat.messages;
 
-    if (!chat) {
-        return res.status(404).json({ error: 'Chat not found'});
-    }
+  if (!chat) {
+    return res.status(404).json({ message: "No chats not found" });
+  }
 
-    res.json(messages);
+  res.json(messages);
 });
 
 // @desc    get message from chat
-// route    GET /chat/:chatId/message/:messageId
+// route    GET /chats/:chatId/messages/:messageId
 const get_message = asyncHandler(async (req, res, next) => {
-    const {chatId, messageId} = req.params;
-    const chat = await Chat.findById(chatId).exec();
-    const message = await Message.findById(messageId).exec();
+  const { chatId, messageId } = req.params;
+  const chat = await Chat.findById(chatId).exec();
+  const message = await Message.findById(messageId).exec();
+  const checkMessageInChat = chat.messages.some(
+    (message) => message.toString() === messageId
+  );
 
-    if (!chat) {
-        return res.status(404).json({ error: 'Chat not found' });
-    }
+  if (!chat) {
+    return res.status(404).json({ error: "Chat not found" });
+  }
 
-    if (!message) {
-        return res.status(404).json({ error: 'Message not found' });
-    }
+  if (!message || !checkMessageInChat) {
+    return res.status(404).json({ error: "Message not found" });
+  }
 
-    res.json(message);
+  res.json(message);
 });
 
 // @desc    add new mesaage to chat
-// route    POST /chat/:chatId/message/:messageId
+// route    POST /chats/:chatId/messages
 const add_message = asyncHandler(async (req, res, next) => {
-    const { chatId } = req.params;
-    const chat = await Chat.findById(chatId);
+  const { chatId } = req.params;
+  const chat = await Chat.findById(chatId);
 
-    if (!chat) {
-        return res.status(404).json({ error: ' Chat not found' });
-    }
+  if (!chat) {
+    return res.status(404).json({ error: " Chat not found" });
+  }
 
-    const newMessage = new Message({
-        sender: req.body.sender,
-        content: req.body.content,
-        chat: req.body.chatId,
-        is_seen: req.body.is_seen,
-        timestamp: req.body.timestamp,
-    });
+  const newMessage = new Message({
+    sender: req.body.sender,
+    content: req.body.content,
+    chat: chatId,
+    is_seen: req.body.is_seen,
+    timestamp: req.body.timestamp,
+  });
 
-    if (!newMessage) {
-        return res.status(404).json({ error: 'Error while adding message' });
-    }
+  if (!newMessage) {
+    return res.status(404).json({ error: "Error while adding message" });
+  }
 
-    chat.messages.push(newMessage._id);
-    console.log(chat.messages);
-    await newMessage.save();
-    await chat.save();
+  chat.messages.push(newMessage._id);
+  console.log(chat.messages);
+  await newMessage.save();
+  await chat.save();
 
-    res.status(201).json({ message: 'Message created', newMessage});
+  res.status(201).json({ message: "Message created", newMessage });
 });
 
 // @desc    edit current message in chat
-// route    PUT /chat/:chatId/message/:messageId
+// route    PUT /chats/:chatId/messages/:messageId
 const edit_message = asyncHandler(async (req, res, next) => {
-    const { chatId, messageId } = req.params;
-    const chat = await Chat.findById(chatId).exec();
-    const message = await Message.findById(messageId).exec();
+  const { chatId, messageId } = req.params;
+  const chat = await Chat.findById(chatId).exec();
+  const message = await Message.findById(messageId).exec();
 
-    if (!chat) {
-        return res.status(404).json({ error: `Chat ID: ${chatId} not found` });
-    }
+  if (!chat) {
+    return res.status(404).json({ error: `Chat ID: ${chatId} not found` });
+  }
 
-    if (!message) {
-        return res.status(404).json({ error: `Message Id; ${messageId} not found` });
-    }
+  if (!message) {
+    return res
+      .status(404)
+      .json({ error: `Message Id; ${messageId} not found` });
+  }
 
-    const updatedMessage = {
-        _id: messageId,
-        sender: req.body.sender,
-        content: req.body.content,
-        chat: chatId,
-        is_seen: req.body.is_seen,
-        timestamp: req.body.timestamp,
-    };
+  const updatedMessage = {
+    _id: messageId,
+    sender: req.body.sender,
+    content: req.body.content,
+    chat: chatId,
+    is_seen: req.body.is_seen,
+  };
 
-    await message.findByIdAndUpdate(messageId, updatedMessage, { new: true });
+  await Message.findByIdAndUpdate(messageId, updatedMessage, { new: true });
 
-    res.json({ message: 'Message updated', updatedMessage});
+  res.json({ message: "Message updated", updatedMessage });
 });
 
 // @desc    delete existing message in chat
-// route    DELETE /chat/:chatId/message/:messageId
+// route    DELETE /chats/:chatId/messages/:messageId
 const delete_message = asyncHandler(async (req, res, next) => {
-    const { chatId, messageId } = req.params;
-    const chat = await Chat.findById(chatId).exec();
-    const message = await Message.findById(messageId).exec();
+  const { chatId, messageId } = req.params;
 
-    if (!chat) {
-        return res.status(404).json({ error: `Chat ID: ${chatId} not found` });
-    }
+  const chat = await Chat.findById(chatId).exec();
+  if (!chat) {
+    return res.status(404).json({ error: `Chat ID: ${chatId} not found` });
+  }
 
-    if (!message) {
-        return res.status(404).json({ error: `Message Id; ${messageId} not found` });
-    }
+  const message = await Message.findById(messageId).exec();
+  if (!message) {
+    return res
+      .status(404)
+      .json({ error: `Message ID: ${messageId} not found` });
+  }
 
-    chat.messages.filter((message) => message != messageId);
-    console.log(chat.messages);
-    const deletedMessage = await Message.findByIdAndDelete(messageId).exec();
+  const deletedMessage = await Message.findByIdAndDelete(messageId).exec();
+  if (!deletedMessage) {
+    return res
+      .status(500)
+      .json({ error: `Error while deleting message with ID: ${messageId}` });
+  }
 
-    if (deletedMessage) {
-        return res.status(404).json({ error: `Error while deleting message with ID: ${messageId}`});
-    }
+  chat.messages = chat.messages.filter(
+    (msgId) => msgId.toString() !== messageId
+  );
+  await chat.save();
 
-    res.json({ deleted: `Message with ID: ${messageId} successfully deleted` });
+  res.json({ message: `Message with ID: ${messageId} successfully deleted` });
 });
 
 export default {
-    get_all_messages,
-    get_message,
-    add_message,
-    edit_message,
-    delete_message,
-}
+  get_all_messages,
+  get_message,
+  add_message,
+  edit_message,
+  delete_message,
+};
--- backend/routes/message.js
+++ backend/routes/message.js
@@ -3,14 +3,14 @@ import messageController from '../controllers/messageController.js';
 
 const router = express.Router();
 
-router.get('/chat/:chatId/messages', messageController.get_all_messages);
+router.get('/chats/:chatId/messages', messageController.get_all_messages);
 
-router.get('/chat/:chatId/message/:messageId', messageController.get_message);
+router.get('/chats/:chatId/messages/:messageId', messageController.get_message);
 
-router.post('/chat/:chatId/message/:messageId', messageController.add_message);
+router.post('/chats/:chatId/messages', messageController.add_message);
 
-router.put('/chat/:chatId/message/:messageId', messageController.edit_message);
+router.put('/chats/:chatId/messages/:messageId', messageController.edit_message);
 
-router.delete('/chat/:chatId/message/:messageId', messageController.delete_message);
+router.delete('/chats/:chatId/messages/:messageId', messageController.delete_message);
 
 export default router;
